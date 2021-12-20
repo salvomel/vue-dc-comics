1a27c4b5b82a35ce62da61fa90c78c1d3f762ce6
salvomel <salv.mel@gmail.com>
2021-12-20 15:55:41 +0100

    footer done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+
+    <main></main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './style/common.scss';
+@import './style/general.scss';
+
+main {
+  // TEST
+  height: 100px;
+  background-color: black;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,114 @@
+<template>
+    <header class="container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="Logo DC">
+        </div>
+
+        <nav>
+            <ul class="menu">
+                <li v-for="(link, index) in links" :key="index" :class="{current: link.current}">
+                    <a :href="link.url">{{link.text}}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data: function() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Tv',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false
+                }
+            ],
+        };
+    }
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+
+    .logo {
+        img {
+            width: 65px;
+        }
+    }
+    
+    .menu {
+        display: flex;
+        list-style-type: none;
+        text-transform: uppercase;
+        font-size: 12px;
+
+        li {
+            margin: 0 10px;
+
+            &.current {
+                color: #0282f9;
+                border-bottom: 4px solid #0282f9;
+            }
+
+            a {
+                line-height: 96px;
+                text-decoration: none;
+                color: black;
+            }
+        }
+    }
+}
+</style>
