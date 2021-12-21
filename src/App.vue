539960cddaaf79d89b8c10a1602f2a6281c33ef6
salvomel <salv.mel@gmail.com>
2021-12-21 16:00:08 +0100

    main structure

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 
     <Banner />
 
-    <main></main>
+    <main>
+      <ComicSeries />
+    </main>
 
     <Prefooter />
 
@@ -18,6 +20,8 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import Prefooter from "./components/Prefooter.vue";
 import Banner from "./components/Banner.vue";
+import ComicSeries from "./components/ComicSeries.vue";
+
 
 export default {
   name: "App",
@@ -25,18 +29,14 @@ export default {
     Header,
     Footer,
     Prefooter,
-    Banner
+    Banner,
+    ComicSeries
   },
 };
 </script>
 
 <style lang="scss">
+
 @import './style/common.scss';
 @import './style/general.scss';
-
-main {
-  // TEST
-  height: 100px;
-  background-color: black;
-}
 </style>
---
+++ src/components/Comic.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="comic">
+        <img :src="comicObject.thumb" :alt="comicObject.series">
+        <h3>{{comicObject.series}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Comic',
+    props: ['comicObject']
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
